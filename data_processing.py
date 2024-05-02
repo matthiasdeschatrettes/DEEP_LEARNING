bf9cd1ffac8d5d3778dfc59ae6435a587fd56e2c
matthiasdeschatrettes <72861966+matthiasdeschatrettes@users.noreply.github.com>
2024-05-02 21:03:05 +0200

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,1 @@
-import pandas as pd
 
-def prepare_data(file_path):
-    # Load the dataset
-    data = pd.read_csv(file_path)
-    # Basic cleaning and processing (à améliorer)
-    # data.dropna(inplace=True)
-    return data
